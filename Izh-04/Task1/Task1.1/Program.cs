ce528e3bfabea727f534ac10a8809c1cf440469f
buain <vbuain@gmail.com>
2020-01-24 00:10:52 +0300

    Izh-04. Task1.1 done

---
+++ Izh-04/Task1/Task1.1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Task1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max_item = 0;
+            int[] arr;
+            Console.WriteLine("Enter how many elements should be in the array:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            arr = new int[n]; //array
+            Random random = new Random();
+
+            Console.WriteLine("Array elements:");
+            for (int i = 0; i<arr.Length; i++) //filling an array with random numbers
+            {
+                arr[i] = random.Next(-100, 100);
+                Console.Write($"{arr[i]}, ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < arr.Length; i++) //Finding the maximum element in an array
+            {
+                if (max_item < arr[i])
+                {
+                    max_item = arr[i];
+                }
+            } 
+            Console.WriteLine($"The maximum element in an array: {max_item}");
+            Console.ReadKey(); // Delay
+        }
+    }
+}
---
+++ Izh-04/Task1/Task1.2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 0;
+            int[] arr = { 2, 5, 9, 8, 7, 3, 4, 1, 7 };
+            for(int i = 0 ; i < arr.Length; i++)
+            {
+                if(index)
+            }
+            Console.WriteLine("Item:{0}");
+            Console.ReadKey(); // Delay
+        }
+    }
+}
