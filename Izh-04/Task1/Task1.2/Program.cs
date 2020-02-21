b714c4e78831f58c51d8ec57daffe557dc4ba0e6
buain <vbuain@gmail.com>
2020-02-21 04:19:36 +0300

    Task Izh-04.1, Izh-04.2 done

--- Izh-04/Task1/Task1.2/Program.cs
+++ Izh-04/Task1/Task1.2/Program.cs
@@ -6,13 +6,42 @@ namespace Task1._2
     {
         static void Main(string[] args)
         {
-            int index = 0;
-            int[] arr = { 2, 5, 9, 8, 7, 3, 4, 1, 7 };
-            for(int i = 0 ; i < arr.Length; i++)
+            int[] arr;
+            //int[] arr = {3, 4, 8, 3, 3, 5, 6, 6, 1 }; // for test
+            int left_sum = 0, right_sum = 0, result = 0;
+            Console.WriteLine("Enter how many elements should be in the array:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            arr = new int[n]; //array
+            Random random = new Random();
+
+            Console.WriteLine("Array elements:");
+            for (int i = 0; i < arr.Length; i++) //filling an array with random numbers
             {
-                if(index)
+                arr[i] = random.Next(-100, 100);
+                Console.Write($"{arr[i]}, ");
             }
-            Console.WriteLine("Item:{0}");
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                right_sum += arr[i];
+            }
+
+            for(int i = 0; i < arr.Length - 1; i++)
+            {
+                if(left_sum == right_sum)
+                {
+                    result = i;                    
+                    break;
+                }                
+                else
+                {
+                    result = -1;
+                }
+                left_sum += arr[i];
+                right_sum -= arr[i + 1];
+            }
+            
+            Console.WriteLine($"Result: {result}");
             Console.ReadKey(); // Delay
         }
     }
--- Izh-04/Task1/Task1.3/Program.cs
+++ Izh-04/Task1/Task1.3/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Task1._3
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string word1 = "Microsoft";
+            string word2 = "Владимир";
+            string result = Add(word1, word2);
+            Console.WriteLine(result);
+
+            Console.ReadKey(); // Delay       
+        }
+
+        ///<summary>
+        ///Алгоритм конкатенации двух строк, содержащих только символы латинского алфавита, исключая повторяющиеся символы
+        ///</summary>
+        ///<param name="first">Первая строка</param>
+        ///<param name="second">Вторая строка</param>
+        private static string Add(string first, string second)
+        {
+            string input = first + second;
+            string output = string.Empty;
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+            foreach (var ch in input)
+            {
+                if (!dict.ContainsKey(ch))
+                {
+                    dict[ch] = 0;
+                    output += ch;
+                }
+            }
+            return output;
         }
     }
 }
